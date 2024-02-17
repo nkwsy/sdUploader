290c9fb9ae6de4eb4281d4cd210e80b65c69ef45
nkwsy <nick@nickwesley.me>
2024-02-16 19:10:55 -0600

    Changed program to work as standalone when calling main. Added daemon to do the drive checking and launching.

--- sdUploader.py
+++ sdUploader.py
@@ -20,7 +20,7 @@ import psutil
 from exiftool import ExifToolHelper
 
 logger.add(sys.stderr, level="INFO")
-logger.add("sdUploader.log", rotation="5 MB")
+logger.add("logs/sdUploader.log", rotation="5 MB")
 load_dotenv()
 
 # Set default values
@@ -335,22 +335,26 @@ def handle_exif_data(img_path):
         # if tag == 'Make' or tag == 'Model':
         #     print(f"{tag}: {value}")
 
-
-
 #TODO Check if exif standardizes date info
 def parse_date(date_string):
     if date_string is None:
         return None
     return datetime.strptime(date_string, '%Y:%m:%d %H:%M:%S')
+
 ############# SD drive util
 #TODO use mountpoint or something else to make sure you do not accidently mount important drive. 
     
 def is_sdX_device(device_string):
+    ### see if device string is a valid sdX device
+    ### DOES NOT WORK FOR /dev/sda as it's the main drive
+    #TODO find if drive is main drive and do not use it w/o manually ommiting sda
+
     # match = re.match(r'/dev/sd[a-z]', device_string)
     match = re.match(r'/dev/sd[b-z]', device_string)
     return match is not None
 
-def check_sd():
+def check_sd(test_dir = NONE):
+    # Get all disk partitions
     dp = psutil.disk_partitions()
     devices = []
     for drive in dp:
